985e425744e9c9ccc223c9eb0096a119606f2c28
Ori06 <oriof2006@gmail.com>
2025-01-26 23:36:42 +0200

    Motorcycle.java

--- src/Motorcycle.java
+++ src/Motorcycle.java
@@ -12,7 +12,7 @@ public class Motorcycle extends Vehicle{
     }
 
     public void fixed() {
-        System.out.println("Car has been repaired. \nAll systems are operational, and the car is ready for a smooth ride.");
+        System.out.println("Motorcycle has been repaired. \nAll systems are operational, and the motorcycle is ready for the road.");
     }
 
     @Override
